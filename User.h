0089502211e2a259ee5351fe23c9a7c3d14f9354
Жека <skydiverjon@icloud.com>
2025-05-06 23:03:11 +0300

    Edite User.h

--- User.h
+++ User.h
@@ -5,12 +5,14 @@ class User {
 	std::string _password;
 	std::string _name;
 public:
-	User(std::string& login,std::string& password,std::string& name): _login(login), _password(password), _name(name){}
+	User(const std::string& login, const std::string& password, const std::string& name) : _login(login), _password(password), _name(name) {}
 	const std::string& getLogin() const { return _login; };
+
 	const std::string& getPassword() const { return _password; };
-	void setPassword(std::string& password) { _password = password; };
+	void setPassword(const std::string& password) { _password = password; };
+
 	const std::string& getName() const { return _name; };
-	void setPassword(std::string& name) { _name = name; };
+	void setName(const std::string& name) { _name = name; };
 
 };
 	
