ba6b48c4c99bc36604c13fbe94f934a5816acde4
Жека <skydiverjon@icloud.com>
2025-05-15 18:49:55 +0300

    Edite Chat.cpp

--- Chat.cpp
+++ Chat.cpp
@@ -4,3 +4,13 @@
 void Chat::start() {
 	_isChatWork = true;
 }
+
+std::shared_ptr<User> Chat::getUserByLogin(const std::string& login) const
+{
+	for (auto& user : _userList)
+	{
+		if (login == user.getLogin())
+			return std::make_shared<User>(user);
+	}
+	return nullptr;
+}
