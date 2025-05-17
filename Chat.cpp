343458bca9e715408161a52b4314f5a3da5ec23c
Жека <skydiverjon@icloud.com>
2025-05-17 22:51:49 +0300

    Edite Chat.cpp

--- Chat.cpp
+++ Chat.cpp
@@ -24,3 +24,29 @@ std::shared_ptr<User> Chat::getUserByLogin(const std::string name) const
 	}
 	return nullptr;
 }
+
+void Chat::login()
+{
+	std::string login;
+	std::string password;
+	char operation;
+
+	do
+	{
+		std::cout << "login";
+		std::cin >> login;
+		std::cout << "password: ";
+		std::cin >> password;
+
+		_currentUser = getUserByLogin(login);
+		if (_currentUser == nullptr || (password != _currentUser->getPassword()))
+		{
+			_currentUser = nullptr;
+			std::cout << "login failed...  (0)exit";
+			std::cin >> operation;
+			if (operation == '0')
+				break;
+		}
+
+	} while (!_currentUser);
+}
